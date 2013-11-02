04be3a216cee67639be61e62c9f80298019a7fa4
Alexander Lang <alex@upstre.am>
2013-11-02 17:04:51 +0100

    adds adding accounts

---
+++ js/app.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict';
+
+  var accounts = new Ractive({
+    el: 'accounts',
+    template: $('#accounts-template').html(),
+    data: {
+      showNew: false,
+      accounts: [],
+      any: function (list) {
+        return list.length;
+      }
+    }
+  });
+  accounts.on('add-account', function(e) {
+    e.original.preventDefault();
+    var $input = $(e.node).parents('form').find('[name=name]');
+    var name = $input.val();
+    $input.val('');
+    accounts.get('accounts').push({name: name, balance: 0});
+    hasher.setHash('/');
+  });
+
+  // routes
+
+  crossroads.addRoute('/', function(){
+    accounts.set('showNew', false);
+  });
+
+  crossroads.addRoute('/accounts/new', function(){
+    accounts.set('showNew', true);
+  });
+
+  function handleChanges(newHash, oldHash){
+    crossroads.parse(newHash);
+  }
+
+  hasher.changed.add(handleChanges);
+  hasher.initialized.add(handleChanges);
+  hasher.init();
+})();
