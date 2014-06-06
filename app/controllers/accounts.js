86a237b0a25d079af2f8c3c4cb2c8e05a05f6748
Alexander Lang <alex@upstre.am>
2014-06-06 22:14:25 +0200

    adds total balance of all accounts

--- app/controllers/accounts.js
+++ app/controllers/accounts.js
@@ -1,3 +1,5 @@
 export default Ember.ArrayController.extend({
-  sortProperties: ['name']
+  sortProperties: ['name'],
+  balances: Ember.computed.mapBy('model', 'balance'),
+  totalBalance: Ember.computed.sum('balances')
 });
