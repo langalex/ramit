a2c0b2d199d036838da6973a803c00956910772c
Alexander Lang <alex@upstre.am>
2014-12-29 22:14:10 +0100

    adds animations

---
+++ app/transitions.js
@@ -0,0 +1,14 @@
+export default function(){
+  this.transition(
+    this.fromRoute('accounts'),
+    this.toRoute('account', 'new_account', 'help'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+  this.transition(
+    this.fromRoute('account'),
+    this.toRoute('new_transaction', 'transaction'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+}
