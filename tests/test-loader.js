8fc44d04ef832f236f3c92cf28ae7d50a858bc60
Alexander Lang <alex@upstre.am>
2018-09-01 22:52:15 +0200

    updates import

--- tests/test-loader.js
+++ tests/test-loader.js
@@ -1,8 +1,8 @@
 /* globals requirejs,require */
-import Ember from 'ember';
+import { keys } from '@ember/polyfills';
 
 // TODO: load based on params
-Ember.keys(requirejs.entries).forEach(function(entry) {
+keys(requirejs.entries).forEach(function(entry) {
   if ((/\-test/).test(entry)) {
     require(entry, null, null, true);
   }
